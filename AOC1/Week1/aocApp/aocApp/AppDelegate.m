db5e0f99ae863cfe4e25d8e92fe76c26c83a4dcc
Adam Du Puis <adamdupuis88@gmail.com>
2013-05-09 19:39:44 -0400

    Assignment Completed
    On to code clean up

--- AOC1/Week1/aocApp/aocApp/AppDelegate.m
+++ AOC1/Week1/aocApp/aocApp/AppDelegate.m
@@ -16,42 +16,98 @@
     [super dealloc];
 }
 
+// Additonal Function For Fun ;)
+int grindTricks = 1;
+
+void myFunctionD (int comboTricks) {
+    comboTricks = comboTricks + 1;
+    grindTricks++;
+    NSLog(@"I am going to do %i combo tricks.", comboTricks);
+    NSLog(@"Then, I am going to do %i grinds tricks.", grindTricks);
+}// END Additonal Function For Fun ;)
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
-    
-
-    // Perform an AND, OR comparison. Use float, int and BOOL types. BOOL values should be YES or NO and written in all caps.
-    // Use an if, else if and else check using any of the data types of your choice.
-    // Perform a single for loop printing out values to the console
-    // Perform a nested loop printing out values to the console
-    // Perform a while loop that increments an int variable and outputs to the console.
-    
+   
     
     NSString * myName = @"Hello, my name is Adam Du Puis.";
     NSLog(@"%@", myName);
-    
-    float  myAge = 24.f;
+ 
+    // Create a variable using the float data type.
+    // Cast the float to an int and using NSLog 
+    float myAge = 24.5f;
     int age = (int) myAge;
     float myNextAge = 25.f;
     int nextAge = (int) myNextAge;
+    // Output both the initial float value as well as the int value.
+    NSLog(@"I turned %d in November.", age);
+    NSLog(@"I am %.1f as of right now.", myAge);
+    NSLog(@"I will be %d years old this November.", nextAge);
     
-    NSLog(@"I am %d years old.", age);
-    NSLog(@"I will be %d years old this year.", nextAge);
+    // Perform an AND, OR comparison. Use float, int and BOOL types. BOOL values should be YES or NO and written in all caps.
+    float skateYes = 10.0;
+    int skateNo = 2.0;
     
-    int tired = 100;
-    int bedTime = 90;
+    BOOL skate = YES;
+    // Use an if, else if and else check using any of the data types of your choice.
+    if ((skate == 12.0) || (skateNo > 4.0)){
+        NSLog(@"NO, I'm not going to skate today.");
+    } else if ((skateNo > 1.0) && (skateYes > 6.0))
+        {
+        NSLog(@"YES, I'm going to skate today.");
+    }
     
-    if ( (tired == 100) || (bedTime > 50) ){
-        NSLog(@"It's time for bed.");
-    } 
+    // Perform a single for loop printing out values to the console
+    
+    //Add single Loop****
+    
+    int impossibleLateFlip = 2;
+    for (int a = 2; a < 5; a++) {
+        NSLog(@"I just landed %i impossible late flips!", impossibleLateFlip);
+        impossibleLateFlip++;
+    }
+    
+    // Perform a while loop that increments an int variable and outputs to the console.
+    
+    //Setup Index
+    int kickFLip = 2;   
+    // Check Condition
+    while ( kickFLip < 5 ) {
+        NSLog(@"I am going to do %i kickflips in a row." ,kickFLip);
+        //Increment of the index
+        kickFLip++;
+    }
+    
+    // Perform a nested loop printing out values to the console  
+    int treFlip = 2;
+    int treFlipSuccess = 2;
+    for (int a = 5; a <= 7; a++) {
+        NSLog(@"I just landed %i tre flips!", treFlip);
+        for (int b = 1; b <= treFlip; b++) {
+            NSLog(@"I landed %i more tre flip successfully!", treFlipSuccess);
+            treFlip++;
+            treFlipSuccess++;
+            break;
+        }
+    }
     
     
+    // Additonal Function For Fun ;)
+    int flipTricks = 1; 
+    for ( int i = 1; i < 2 ; i++) {
+        int flipTricks = 1;
+        grindTricks++;
+        myFunctionD(flipTricks);
+        NSLog(@"I have landed %i flip trick.", flipTricks);
+    }
+    NSLog(@"I have not landed %i flip trick.", flipTricks);
+    // END Additonal Function For Fun ;)
+    return YES;
 }
-
 - (void)applicationWillResignActive:(UIApplication *)application
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
