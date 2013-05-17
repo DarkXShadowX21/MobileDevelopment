2f027f45096c57c532579bf652e658c9de804cca
Adam Du Puis <adamdupuis88@gmail.com>
2013-05-16 20:30:51 -0400

    AOC1 Week 2 Cleaned Up & Organized Code.
    FINISHED

--- AOC1/Week2/aocApp2/aocApp2/ViewController.m
+++ AOC1/Week2/aocApp2/aocApp2/ViewController.m
@@ -16,37 +16,23 @@
 
 - (void)viewDidLoad
 {
+    //BG Color
+    self.view.backgroundColor = [UIColor blackColor];//BG Color Closed
     
-// INITIAL AOC1 WEEK 2 COMMIT
-    
-//    Instructions
-//    
-//    In this example you will be utilizing the information you've learned about UILabels, NSString and arrays. You will need to choose a book that you have read and use it as the source information for this project.
-//    
-//    Make sure that ALL the colors you use are different, if you have 8 labels you will use 16 colors, 9 labels=18 colors, and so on. All text will need to be readable so choose the colors wisely. All UI elements must be created in code and not using Interface Builder at this time. We do this so you understand how UI creation works in detail.
-//        
-//        - View
-//        Change the background color of the view to any color you wish.
-    
-    self.view.backgroundColor = [UIColor whiteColor];
-    
-    
-//        
-//        - Book Information and UILabels
-//        Create a UILabel and place it at the top of the view. This is used as the title of the book. Center the text for this label.
+    //1st UILabel Book Title "The Wolf Gift"
     
     bookTitle = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, 0.0f, 120.0f, 460.0f)];
     if (bookTitle != nil)
     {
-            bookTitle.text = @"The Wolf Gift";
-            bookTitle.textAlignment = NSTextAlignmentCenter;
-            bookTitle.textColor = [UIColor colorWithRed:0.804 green:0.522 blue:0.247 alpha:1];
-            bookTitle.backgroundColor = [UIColor colorWithRed:0.545 green:0.271 blue:0.075 alpha:1]; /*#8b4513*/
+        bookTitle.text = @"The Wolf Gift";
+        bookTitle.textAlignment = NSTextAlignmentCenter;
+        bookTitle.textColor = [UIColor colorWithRed:0.804 green:0.522 blue:0.247 alpha:1];
+        bookTitle.backgroundColor = [UIColor colorWithRed:0.545 green:0.271 blue:0.075 alpha:1]; /*#8b4513*/
     }
     
-    [self.view addSubview:bookTitle];
+    [self.view addSubview:bookTitle];//1st UILabel "The Wolf Gift" Closed
     
-//            Create another label that contains the text "Author:" with the text right justified.
+    //2nd UILabel Author "Author: "
     
     author = [[UILabel alloc] initWithFrame:CGRectMake(120.0f, 0.0f, 100.0f, 20.0f)];
     if (author != nil)
@@ -57,11 +43,9 @@
         author.backgroundColor = [UIColor colorWithRed:0.855 green:0.647 blue:0.125 alpha:1]; /*#daa520*/
     }
     
-    [self.view addSubview:author];
-
-            
-//            Create a UILabel to the right of the author title label and have it contain the name of the author of the book you chose. This text will be left justified.
+    [self.view addSubview:author];//2nd UILabel Author "Author: " Closed
     
+    //3rd UILabel Author Name "Anne Rice"
     authorName = [[UILabel alloc] initWithFrame:CGRectMake(220.0f, 0.0f, 100.0f, 20.0f)];
     if (authorName != nil)
     {
@@ -71,9 +55,9 @@
         authorName.backgroundColor = [UIColor colorWithRed:0.482 green:0.408 blue:0.933 alpha:1]; /*#7b68ee*/
     }
     
-    [self.view addSubview:authorName];
+    [self.view addSubview:authorName];//3rd UILabel Author Name "Anne Rice"   Closed
     
-//            Create a UILabel on the next line called "Published:". This text is right justified.
+    //4th UILabel Published "Published: "
     
     published = [[UILabel alloc] initWithFrame:CGRectMake(120.0f, 20.0f, 100.0f, 20.0f)];
     if (published != nil)
@@ -84,9 +68,9 @@
         published.backgroundColor = [UIColor colorWithRed:0.294 green:0 blue:0.51 alpha:1]; /*#4b0082*/
     }
     
-    [self.view addSubview:published];
+    [self.view addSubview:published];//4th UILabel Published "Published: " Closed
     
-//            Create a label next to the Published label and add the text of when the book was published. This text is left justified.
+    //5th UILabel Publish Year "2012"
     
     year = [[UILabel alloc] initWithFrame:CGRectMake(220.0f, 20.0f, 100.0f, 20.0f)];
     if (year != nil)
@@ -97,9 +81,9 @@
         year.backgroundColor = [UIColor colorWithRed:0 green:0.4 blue:1 alpha:1]; /*#0066ff*/
     }
     
-    [self.view addSubview:year];
+    [self.view addSubview:year];//5th UILabel Publish Year "2012" Closed
     
-//            Create a UILabel with the text "Summary". This text is left justified.
+    //6th UILabel Summary
     
     summary = [[UILabel alloc] initWithFrame:CGRectMake(120.0f, 40.0f, 100.0f, 20.0f)];
     if (summary != nil)
@@ -110,9 +94,9 @@
         summary.backgroundColor = [UIColor colorWithRed:0.698 green:0 blue:0 alpha:1]; /*#b2000*/
     }
     
-    [self.view addSubview:summary];
+    [self.view addSubview:summary]; //6th UILabel Summary Closed
     
-//            Create another UILabel that contains a small summary of the book's plot. This text is centered and should span multiple lines.
+    //7th UILabel Plot
     
     plot = [[UILabel alloc] initWithFrame:CGRectMake(120.0f, 60.0f, 200.0f, 290.0f)];
     if (plot != nil)
@@ -124,11 +108,9 @@
         plot.numberOfLines = 15;
     }
     
-    [self.view addSubview:plot];
+    [self.view addSubview:plot];//7th UILabel Plot Closed
     
-//            - Labels and Arrays
-    
-//            Create a label with the text "List of items" and add it to the parent view. Make sure the text is left justified.
+    //List Of Items
     
     bookItems = [[UILabel alloc] initWithFrame:CGRectMake(120.0f, 350.0f, 100.0f, 20.0f)];
     if (bookItems != nil)
@@ -139,9 +121,9 @@
         bookItems.backgroundColor = [UIColor colorWithRed:1 green:0 blue:0.4 alpha:1]; /*#ff0066*/
     }
     
-    [self.view addSubview:bookItems];
+    [self.view addSubview:bookItems];//List Of Items Closed
     
-//            Create an NSArray of 5 items talked about in the book. These items will be NSStrings. Add the items to the array
+    //Multiple NSArray's
     
     bookItem1 = @"Relationships";
     bookItem2 = @"Reporter";
@@ -149,21 +131,19 @@
     bookItem4 = @"Ruebon";
     bookItem5 = @"Mansion";
     
-    bookEvents = [[NSArray alloc] initWithObjects: bookItem1, bookItem2, bookItem3, bookItem4, bookItem5, nil];
+    bookEvents = [[NSArray alloc] initWithObjects: bookItem1, bookItem2, bookItem3, bookItem4, bookItem5, nil];//Multiple NSArray's Closed
     
-//            Create a variable of type NSMutableString and allocate it. Loop through the NSArray you created and append each of these items to your NSMutableString     separated by commas. The last item should be preceded by an "and" (For example: dinosaurs, jeeps, storm, giant turkeys, and eating people).
-           
+    // Looping through NSMutableString
+    
     allBookEvents = [[NSMutableString alloc] initWithCapacity:5];
     for ( int i = 0; i < bookEvents.count; i++){
         [allBookEvents appendString: [bookEvents objectAtIndex: i]];
         if ( i < bookEvents.count - 1){
             [allBookEvents appendString: @","];
         }
-    }
+    }// Looping through NSMutableString Closed
     
-    
-//            Create another label beneath and set the text to the NSMutableString text. Increase the number of lines if necessary. Make sure the text in this label is centered
-    
+    // Increasing the lines in NSMutableString text.
     
     allBookItems = [[UILabel alloc] initWithFrame:CGRectMake(120.0f, 370.0f, 200.0f, 90.0f)];
     if (bookItems != nil)
@@ -175,10 +155,7 @@
         allBookItems.numberOfLines = 15;
     }
     
-    [self.view addSubview:allBookItems];
-    
-    
-    
+    [self.view addSubview:allBookItems];// Increasing the lines in NSMutableString text. Closed
     
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
