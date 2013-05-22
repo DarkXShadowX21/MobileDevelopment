415eadac26df261e4ee514dc543ab2853d7ed7e6
DarkXShadowX21 <adamdupuis88@gmail.com>
2013-05-22 17:01:11 -0400

    AOC1 Initial Week 3 Commit

---
+++ AOC1/Week3/aocApp3/aocApp3/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  aocApp3
+//
+//  Created by Adam on 5/22/13.
+//  Copyright (c) 2013 Adam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AOC1/Week3/aocApp3/aocApp3/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  aocApp3
+//
+//  Created by Adam on 5/22/13.
+//  Copyright (c) 2013 Adam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
