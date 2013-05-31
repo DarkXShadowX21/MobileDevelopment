d642ac4f80c4982d5939d57818699f95f63fbca0
DarkXShadowX21 <adamdupuis88@gmail.com>
2013-05-30 23:49:22 -0400

    Deleted Intructions

--- AOC1/Week4/aocApp4/aocApp4/ViewController.m
+++ AOC1/Week4/aocApp4/aocApp4/ViewController.m
@@ -78,50 +78,11 @@
     }
     
     
-          
+    
 }
 
 - (void)viewDidLoad
 {
-    //    LOGIN
-    //
-    //    1. Create a UILabel near the top of your screen with the text "Username:" in it. [COMPLETED]
-    //
-    //    2. Create a UITextField to the right of the username label [COMPLETED]
-    //
-    //    3. Create a rounded rectangle UIButton of any color under the UITextField with the text "Login" on it. [COMPLETED]
-    //
-    //    4. Create another UILabel beneath with the default text "Please Enter Username". [COMPLETED]
-    //
-    //    5. Add a target to the UIButton to call a function called onClick when the user presses the Login button. [COMPLETED]
-    //
-    //    6. If the user has not entered any text into the UITextField, display in the UILabel, "Username cannot be empty". Otherwise, display "User: username has been logged in". [COMPLETED]
-    //
-    //    Hint: NSString has a property called length that tells you how many characters are in the string.
-    //
-    //    DATE
-    //
-    //    1. This section will display a UIAlertView with the current date and time in it using an NSDate object.
-    //
-    //    2. Create a UIButton using the rounded rectangle type. Give this button any color you wish.
-    //
-    //    3. Add the text "Show Date" to the button
-    //
-    //    4. Add an action to the button that when clicked, it will call the same onClick handler you already defined. Make sure to add a tag to the date button so you know which one was pressed.
-    //
-    //    5. Display a UIAlertView with the current date and time displayed in the format seen in the dateAlert graphic in the assets section of this project assignment. You can either format the date and time manually or use the date and time styles. You must use an NSDate object to gather the date and time information.
-    //
-    //    INFORMATION
-    //
-    //    1. This section will display the text "This application was created by: Firstname Lastname" in a label when the info button is clicked.
-    //
-    //    2. Create a UIButton using either the light or dark info type and position it somewhere near the bottom of the screen.
-    //
-    //    3. Create a UILabel beneath it that contains no initial text.
-    //    Hook up an action to the info button to have it call the onClick handler you created earlier.
-    //
-    //    4. When the button is pressed, have the text "This application was created by: Firstname Lastname" appear in the info UILabel. Please replace firstname lastname with your name.
-    
     
     //BG Color
     self.view.backgroundColor = [UIColor brownColor];
